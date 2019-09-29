2617985bb42f931beb84efa5bc3533f0bec59d19
AlexSikand <alex@sikand.org>
2019-09-29 00:56:04 -0400

    Remove PS1 from PS2 branch

--- PS1/PS1.P1.js
+++
@@ -1,5 +0,0 @@
-//P1.js
-//Function to reorder string alphabetically
-const inOrder = s => s.split('').sort().join('')
-console.log(`Alphabetically Ordered String: ${inOrder("bcad")}`)
-//Alphabetically Ordered String: abcd
