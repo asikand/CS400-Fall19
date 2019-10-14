e3de2f4e67f23ecd3c61146f63e8efbdef3dd516
AlexSikand <alex@sikand.org>
2019-10-13 19:28:17 -0700

    First commit for PS3 (missing Pug template)

--- PS2/PS2.P3.js
+++
@@ -1,8 +0,0 @@
-//Use ES6 arrow notation to create function that returns the cube value of its input
-const cubeVal = (x) => { return Math.pow(x, 3) };
-
-//Example provided by problem set
-var hwTest = [1,2,3,4,5,6,7];
-
-//Use 1-line map command to cube the elements of the array
-console.log(hwTest.map(cubeVal))
